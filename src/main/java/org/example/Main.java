269414500bfa99ca956f9b2cdf37b31c8480314c
JalenEvans <jalenemmanuelevans@gmail.com>
2022-11-07 00:43:55 -0500

    Made the Pokemon class and its constructor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class Main {
+
+
     public static void main(String[] args) {
+        Pokemon pokemon = new Pokemon(11, "fire", 11.3, 15.6);
+
         System.out.println("Hello world!");
+
+        pokemon.writeToFile(pokemon);
     }
 }
--- src/main/java/org/example/Pokemon.java
+++ src/main/java/org/example/Pokemon.java
@@ -1,15 +1,17 @@
+package org.example;
+
 import java.nio.*;
 
 public class Pokemon {
 
+    //Declaring the descriptive variables for the Pokemon object
     int level;
     String type;
     double height;
     double weight;
+    public Pokemon (int level, String type, double height, double weight) {
 
-
-    public Pokemon(int level, String type, double height, double weight) {
-
+        //Intializing the descriptive variables for the Pokemon object
         this.level = level;
         this.type = type;
         this.height = height;
@@ -17,5 +19,10 @@ public class Pokemon {
 
     }
 
-    public static void
+    //Method that writes the components of the object to a csv file
+    public static void writeToFile(Pokemon pokemon) {
+
+        System.out.print("Giraffe");
+    }
+
 }
